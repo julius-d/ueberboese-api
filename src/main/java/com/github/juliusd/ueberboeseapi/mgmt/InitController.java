99b7fcc86241da5a0cd658161e703b425f3cbbaa
Julius <30121440+julius-d@users.noreply.github.com>
2026-06-05 20:02:27 +0200

    feat(bmx): derive base URL from request instead of config

--- src/main/java/com/github/juliusd/ueberboeseapi/mgmt/InitController.java
+++ src/main/java/com/github/juliusd/ueberboeseapi/mgmt/InitController.java
@@ -1,25 +1,22 @@
 package com.github.juliusd.ueberboeseapi.mgmt;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/mgmt/init")
 public class InitController {
 
   @GetMapping(value = "/set-up-this-speaker.sh", produces = MediaType.TEXT_PLAIN_VALUE)
-  public ResponseEntity<String> setupScript(HttpServletRequest request) throws IOException {
-    String scheme = Optional.ofNullable(request.getHeader("X-Forwarded-Proto")).orElse("http");
-    String host = request.getHeader("Host");
-    String baseUrl = scheme + "://" + host;
+  public ResponseEntity<String> setupScript() throws IOException {
+    String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
 
     ClassPathResource resource = new ClassPathResource("templates/set-up-this-speaker.sh");
     String template = resource.getContentAsString(StandardCharsets.UTF_8);
